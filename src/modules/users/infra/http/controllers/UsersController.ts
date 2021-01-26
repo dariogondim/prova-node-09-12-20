d3a4f55838ccba77cbb2e4dbd54049f50b05cbdd
Dario gondim <cognusdev@gmail.com>
2021-01-26 17:41:34 -0300

    add implementation of create users

--- src/modules/users/infra/http/controllers/UsersController.ts
+++ src/modules/users/infra/http/controllers/UsersController.ts
@@ -1,13 +1,27 @@
 import { Request, Response } from 'express';
 
 import { container } from 'tsyringe';
+import CreateUserService from '../../../services/CreateUserService';
 
 export default class UsersControllers {
     public async create(
         request: Request,
         response: Response,
     ): Promise<Response> {
-        return response.json();
+        const { name, username, email, password } = request.body;
+
+        const createUser = container.resolve(CreateUserService);
+
+        const { user } = await createUser.execute({
+            name,
+            username,
+            email,
+            password,
+        });
+
+        delete user.password;
+
+        return response.json({ user });
     }
 
     public async readAll(
